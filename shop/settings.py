484c9ba3221268a6a5953ecaa7a7799a5a7a3705
Rodanger <rodangerrm@gmail.com>
2022-12-20 22:34:31 -0500

    first time

--- shop/settings.py
+++ shop/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,7 +34,7 @@ SECRET_KEY = 'django-insecure-c0d0b@1_*6pd)mo3+wz@*(9+on^a-f9(se*eowg56&^ecg976)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -134,7 +137,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-
+django_heroku.settings(locals())
 
 
 
